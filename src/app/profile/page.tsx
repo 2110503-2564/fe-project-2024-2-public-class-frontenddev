0008c2766dafc8f0e8c842e91af609b2c9a00a53
NarawitPisaspongsa <6733123921@student.chula.ac.th>
2025-03-25 20:48:49 +0700

    styling home

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 export default function ManageBooking() {
     return (
         <main>
-            <div className="text-center text-lg">
+            <div className="text-center text-3xl font-bold py-3">
                 Profile
             </div>
         </main>
