60a81c82d3194f292aa9b12f3af5ff8bbf5bca87
NarawitPisaspongsa <6733123921@student.chula.ac.th>
2025-03-25 22:01:09 +0700

    title font change and the welcome changej

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -21,13 +21,13 @@ export default function Banner() {
             priority
             objectFit='cover'/>
             <div className={styles.bannerText}>
-                <h1 className='px-5 py-8 rounded-2xl outline-1 outline-gray-300 inline-block text-6xl font-medium text-emerald-700 bg-pink-100 bg-opacity-25 backdrop-blur-sm'>Find The Right Place For Camping</h1>
+                <h1 className='px-5 py-8 rounded-2xl outline-1 outline-gray-300 inline-block text-6xl font-serif font-extrabold text-emerald-700 bg-pink-100 bg-opacity-25 backdrop-blur-sm'>Find The Right Place For Camping</h1>
             </div>
             {
                 session? <div className='z-30 absolute top-5
-                right-10 font-semibold text-white text-3xl'>Welcome {session.user?.name}</div>:null
+                right-10 text-3xl font-mono font-light bg-slate-600 px-3 py-4 bg-opacity-10 rounded-md'>Welcome {session.user?.name}</div>:null
             }
-            <button className='bg-white text-green-600 border border-green-600
+            <button className='bg-white text-green-600 border-2 border-green-600
             font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0
             hover:bg-green-600 hover:text-white hover:border-transparent'
             onClick={(e)=>{ e.stopPropagation();
